c555b076eb8596be0b0659042e7975db09388bd6
Thomas Hildmann <hildi@hildania.de>
2023-04-16 08:30:54 +0200

    added comments and tutorial references to QtGridLayout() demo

--- gridtest.py
+++ gridtest.py
@@ -7,11 +7,18 @@ from TouchStyle import *
 class FtcGuiApplication(TouchApplication):
     def __init__(self, args):
         TouchApplication.__init__(self, args)
-        mtext = []
-        mvalue = []
+
+        mtext = []  # Text für Spalte links
+        mvalue = [] # Werte in der Spalte rechts
+
+        # Raster erstellen
+        # https://www.tutorialspoint.com/pyqt/pyqt_qgridlayout_class.htm
         grid = QGridLayout()
 
+        # 5 Zeilen: Motoren 0..4 und ein Greifer
         for i in range(5):
+            # Labels erzeugen
+            # https://www.tutorialspoint.com/pyqt/pyqt_qlabel_widget.htm
             mtext.append(QLabel())
             if i == 4:
                 mtext[i].setText('GRAB=')
@@ -21,17 +28,20 @@ class FtcGuiApplication(TouchApplication):
             mvalue.append(QLabel())
             mvalue[i].setText('0')
             mvalue[i].setAlignment(Qt.AlignRight)
-
+            # in Raster einfügen
             grid.addWidget(mtext[i], i, 1)
             grid.addWidget(mvalue[i], i, 2)
 
+        # ganz unten kommt noch ein Statustext hin
         status = QLabel()
         status.setText('Testing...')
         status.setAlignment(Qt.AlignCenter)
-
+        # Platzierung Zeile 5, Spalte 1 aber über 2 Spalten breit
         grid.addWidget(status, 5, 1, 1, 2)
 
         w = TouchWindow("Grid+")
+        # Layout auf das erstellte Raster ändern
+        # https://cfw.ftcommunity.de/ftcommunity-TXT/en/programming/python/tutorial-3.html
         w.centralWidget.setLayout(grid)
         w.show()
         self.exec_()
