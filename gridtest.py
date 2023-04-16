43d9bb3697fd7956c938ae3571832c8e6bcc7be4
Thomas Hildmann <hildi@hildania.de>
2023-04-16 11:01:44 +0200

    changed layout to 4x4 grid and added button to end recording

--- gridtest.py
+++ gridtest.py
@@ -16,30 +16,43 @@ class FtcGuiApplication(TouchApplication):
         grid = QGridLayout()
 
         # 5 Zeilen: Motoren 0..4 und ein Greifer
-        for i in range(5):
+        for i in range(6):
             # Labels erzeugen
             # https://www.tutorialspoint.com/pyqt/pyqt_qlabel_widget.htm
             mtext.append(QLabel())
             if i == 4:
-                mtext[i].setText('GRAB=')
+                mtext[i].setText('grab:')
+            elif i == 5:
+                mtext[i].setText('steps:')
             else:
                 mtext[i].setText('M'+str(i)+'=')
             mtext[i].setAlignment(Qt.AlignLeft)
             mvalue.append(QLabel())
             mvalue[i].setText('0')
-            mvalue[i].setAlignment(Qt.AlignRight)
+            mvalue[i].setAlignment(Qt.AlignLeft)
             # in Raster einfügen
-            grid.addWidget(mtext[i], i, 1)
-            grid.addWidget(mvalue[i], i, 2)
+            if (i % 2) == 0:
+                # ungerade links
+                grid.addWidget(mtext[i], int(i / 2), 1)
+                grid.addWidget(mvalue[i], int(i / 2), 2)
+            else:
+                # gerade rechts
+                grid.addWidget(mtext[i], int(i / 2), 3)
+                grid.addWidget(mvalue[i], int(i / 2), 4)
 
         # ganz unten kommt noch ein Statustext hin
         status = QLabel()
         status.setText('Testing...')
         status.setAlignment(Qt.AlignCenter)
-        # Platzierung Zeile 5, Spalte 1 aber über 2 Spalten breit
-        grid.addWidget(status, 5, 1, 1, 2)
+        # Platzierung Zeile 5, Spalte 1 aber über 4 Spalten breit
+        grid.addWidget(status, 6, 1, 1, 4)
 
-        w = TouchWindow("Grid+")
+        # Knopf zum Beenden
+        endbutton = QPushButton("Record Stop")
+        # auch über die ganze Breite
+        grid.addWidget(endbutton, 7, 1, 1, 4)
+
+        w = TouchWindow("Teach In")
         # Layout auf das erstellte Raster ändern
         # https://cfw.ftcommunity.de/ftcommunity-TXT/en/programming/python/tutorial-3.html
         w.centralWidget.setLayout(grid)
